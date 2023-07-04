4b643c5becf1f83ae6fe6d394b5dd368a3e180d3
Georgios Kostaras <ksrgeorge@gmail.com>
2023-07-04 15:58:17 +0000

    Update 2 files
    - /terraform/main.tf
    - /.gitlab-ci.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,23 +45,6 @@ resource "aws_s3_bucket_acl" "test-npm-bucket" {
   acl    = "public-read-write"
 }
 
-#resource "aws_kms_key" "mykey" {
-#  description             = "This key is used to encrypt bucket objects"
-#  deletion_window_in_days = 10
-#}
-
-#resource "aws_s3_bucket_server_side_encryption_configuration" "test-npm-bucket" {
-#  bucket = aws_s3_bucket.test-npm-bucket.id
-
-#  rule {
-#    apply_server_side_encryption_by_default {
-#      kms_master_key_id = aws_kms_key.mykey.arn
-#      sse_algorithm     = "aws:kms"
-#    }
-#    bucket_key_enabled = true
-#  }
-#}
-
 resource "aws_s3_bucket_policy" "test-npm-bucket" {
   bucket = aws_s3_bucket.test-npm-bucket.id
   policy = data.aws_iam_policy_document.test-npm-bucket.json
